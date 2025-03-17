9fe059ae4ffd87e622d5ba5ca6cf871756d885cb
Abyalew <labyalew@gmail.com>
2025-03-17 17:22:18 +0300

    complete image uplode functiona;ity

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,89 @@
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-
+import { useRef } from "react";
+import { account, storage } from "../appWrite/appwriteConfig";
+import { Await } from "react-router-dom";
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [imageUrl, setImageUrl] = useState("");
+  const [formData, setFormData] = useState({});
+  const [isUpplodeE, setIsUplodeE] = useState(false);
+
+  console.log(file);
+  console.log(formData);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      console.log(file);
+    }
+  };
+
+  const handleFileUpload = async (file) => {
+    if (!file) {
+      console.error("No file selected.");
+      return;
+    }
+
+    try {
+      const fileId = `unique()`; // Generates a unique ID
+      const response = await storage.createFile(
+        "67d6b927001599b4e502",
+        fileId,
+        file
+      );
+      console.log("Upload successful:", response);
+
+      const imageUrl = storage.getFilePreview(
+        "67d6b927001599b4e502",
+        response.$id
+      );
+      setImageUrl(imageUrl);
+
+      console.log(imageUrl);
+      setFormData({ ...formData, avatar: imageUrl });
+    } catch (error) {
+      console.error("Upload failed:", error);
+      setIsUplodeE(true);
+    }
+  };
   return (
     <div className="max-w-lg mx-auto p-3">
       <h1 className="text-3xl font-semibold my-7 text-center">Profile</h1>
-      <form className="flex flex-col gap-4">
-        <img
-          src={currentUser.avatar}
-          alt="profiel"
-          className="rounded-full w-24 h-24 object-cover cursor-pointer self-center mt-2"
+      <form className="flex flex-col gap-4" onSubmit={handleFileUpload}>
+        <input
+          onChange={handleFileChange}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
         />
+        <img
+          onClick={() => fileRef.current.click()}
+          className="rounded-full h-24 w-24 self-center object-cover cursor-pointer"
+          src={imageUrl || currentUser.avatar}
+          alt="profile"
+        />
+        <p className="text-center self-center">
+          {imageUrl ? (
+            <span className="text-green-700">
+              Image Uplode Successfully Uploaded!
+            </span>
+          ) : isUpplodeE ? (
+            <span className="text-red-700"> ErrorImage Uplode</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
