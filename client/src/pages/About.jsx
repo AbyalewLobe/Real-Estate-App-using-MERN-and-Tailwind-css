bd09a5d3f70c71193c1f625737dc8c410e735b01
Abyalew <labyalew@gmail.com>
2025-04-25 15:22:06 +0300

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Abyalew Estate
+      </h1>
+      <p className="mb-4 text-slate-600">
+        Abyalew Estate is a modern real estate platform committed to helping
+        individuals and families find their ideal homes with confidence and
+        ease. Whether you're looking to buy, sell, or rent, our mission is to
+        make every step of your property journey simple, secure, and satisfying.
+      </p>
+      <p className="mb-4 text-slate-700">
+        We offer a wide selection of verified listings—from cozy apartments to
+        luxurious villas—all tailored to suit different lifestyles and budgets.
+        With a user-friendly interface, powerful search tools, and expert
+        support, Abyalew Estate takes the stress out of property hunting and
+        makes it a seamless experience.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Abyalew Estate, we believe that finding a home shouldn't be
+        complicated. It should feel exciting, empowering, and, most of all,
+        possible. Welcome to the place where your next chapter begins.
+      </p>
+    </div>
+  );
 }
