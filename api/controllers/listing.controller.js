3865efd481ae1f0b8f5f13f6adfb48b4ea1f50a5
Abyalew <labyalew@gmail.com>
2025-04-25 09:42:40 +0300

    add onChange and onSubmit functionality to the search page and fix sometings in listing controller

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -61,7 +61,7 @@ export const getListing = async (req, res, next) => {
 export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
-    const startIndex = parseInt(req.query.startIndex) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
     if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
@@ -80,7 +80,7 @@ export const getListings = async (req, res, next) => {
     }
     const searchTerm = req.query.searchTerm || "";
 
-    const sort = req.query.order || "createdAt";
+    const sort = req.query.sort || "createdAt";
     const order = req.query.order || "desc";
     const listings = await Listing.find({
       name: { $regex: searchTerm, $options: "i" },
