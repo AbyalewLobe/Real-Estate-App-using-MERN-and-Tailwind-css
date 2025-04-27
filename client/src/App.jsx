6f63c9ed71af885fa881b704d2afac26741b1f57
Abyalew <labyalew@gmail.com>
2025-04-27 12:00:24 +0300

    fix App for profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
